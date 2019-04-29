63d2bf2c355b1cfce12673407fed0af78d415540
Elihle <lihlemenz@gmail.com>
2019-04-29 15:06:09 +0200

    initial commit

---
+++ src/bicycles/Bicycle.java
@@ -0,0 +1,30 @@
+package bicycles;
+
+public class Bicycle {
+
+    public void accellerate () {
+        //increase speed
+
+        int speed = 5;
+        while (speed < 5) {
+            speed++;
+        }
+    }
+
+
+//    public void brake () {
+//    decrease speed
+
+//        int speed = 3;
+//    }
+//
+//    public void currentSpeed () {
+//    current speed
+//    }
+//
+//    public void stop () {
+//    returns 0
+//        int speed = 0;
+//
+//    }
+}
